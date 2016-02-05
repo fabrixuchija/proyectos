d4d5c31b9b3d00b5d2ed6bc2d6964e576e2f5f53
fabrixuchija <fabrixuchija@gmail.com>
2016-02-05 11:31:55 -0600

    subido

---
+++ Reto1Solution/Reto1/scripts/compra.js
@@ -0,0 +1,50 @@
+﻿var m = 0;
+var p = 0;
+var n = 0;
+var na = 12;
+var man = 3;
+var pe = 7;
+
+function initialize() {
+    document.getElementById('btnN')
+        .addEventListener('click', addN, false);
+    document.getElementById('btnP')
+    .addEventListener('click', addP, false);
+    document.getElementById('btnM')
+    .addEventListener('click', addM, false);
+    document.getElementById('maS').innerText = man;
+    document.getElementById('peS').innerText = pe;
+    document.getElementById('naS').innerText = na;
+}
+
+function addN(){
+    if (na > 0) {
+        n++;
+        na--;
+        document.getElementById('naC').innerText = n;
+        document.getElementById('naS').innerText = na;
+    } else
+        alert('ya no puedes comprar naranjas');
+}
+
+function addM() {
+    
+    
+    if (man > 0) {
+        m++;
+        man--;
+        document.getElementById('maC').innerText = m;
+        document.getElementById('maS').innerText = man;
+    } else
+        alert('ya no puedes comprar manzanas');
+}
+
+function addP(){
+     if (pe > 0) {
+    p++;
+    pe--;
+    document.getElementById('peC').innerText = p;
+    document.getElementById('peS').innerText = pe;
+} else
+        alert('ya no puedes comprar peras');
+}
---
+++ WebApplication5/WebApplication5/scripts/default.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="jquery-1.12.0.js" />
+
+$(document).on('ready', function () {
+    var as;
+    $('#cantidad').keypress(function (e) {
+
+
+        as = $('#cantidad').val();
+        if (as > (12-$('.comprado').length)) {
+            alert('a exedido el numero de asientos disponibles');
+            as = 0;
+        }
+
+    });
+
+
+    $('td').on('click', function () {
+        if (!$(this).hasClass('comprado') && (as > $('.selected').length || $(this).hasClass('selected'))) {
+            $(this).toggleClass('selected');
+            
+        }
+    });
+
+    $('#btnComprar').on('click', function () {
+        if ($('td').hasClass('selected')) {
+
+            $('td.selected').addClass('comprado');
+            $('td.selected.comprado').removeClass('selected');
+
+            as = 0;
+        }
+    });
+});
+
